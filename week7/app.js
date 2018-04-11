6aee7562c2d6abd9c24627ae2418c205c90d60d5
Vaibhav Gupta <33783341+Vaibhavg97@users.noreply.github.com>
2018-04-11 10:50:15 +0530

    Update app.js

--- week7/app.js
+++ week7/app.js
@@ -43,11 +43,11 @@ function showScores() {
 
 // create questions
 var questions = [
-    new Question("Which one is not an object oriented programming language?", ["Java", "C#","C++", "C"], "C"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("There are ____ main components of object oriented programming.", ["1", "6","2", "4"], "4"),
-    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
-    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
+    new Question("Grand Central Terminal, Park Avenue, New York is the world's ", ["largest railway station", "highest railway station","longest railway station", "None of the above"], "longest railway station"),
+    new Question("Garampani sanctuary is located at ", ["Junagarh, Gujarat", "Diphu, Assam", "Kohima, Nagaland", "Gangtok, Sikkim"], "Kohima, Nagaland"),
+    new Question("FFC stands for ", ["Foreign Finance Corporation", "Film Finance Corporation","Federation of Football Council", "None of the above"], "Film Finance Corporation"),
+    new Question("Fastest shorthand writer was", ["Dr. G. D. Bist", "J.R.D. Tata", "J.M. Tagore", "Khudada Khan"], "Dr. G. D. Bist"),
+    new Question("Galileo was an Italian astronomer who", ["developed the telescope", "discovered four satellites of Jupiter", "discovered that the movement of pendulum produces a regular time measurement", "All of the above"], "All of the above")
 ];
 
 // create quiz
